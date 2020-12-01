68cd627f0ef57a5604ac1f948df3020810d7113b
Marcythm <marcythm@gmail.com>
2020-12-01 16:30:46 +0800

    rename header file and source file

--- src/uinf.cpp
+++ src/uinf.cpp
@@ -1,9 +1,9 @@
-#include "u32.hpp"
+#include "uinf.hpp"
 
 // ---------- constructors and assignment operators ----------
 
 template <typename T>
-u::u(T val) {
+uinf::uinf(T val) {
 	if constexpr (std::is_integral<T>::value) {
 		val = std::abs(val);
 		do {
@@ -12,35 +12,35 @@ u::u(T val) {
 		} while (val > 0);
 	} else throw "constructing an unsigned integer with an non-integral value";
 }
-u::u(const char *s) {
+uinf::uinf(const char *s) {
 	value.insert(value.end(), s, s + strlen(s));
 	auto pos = value.begin();
 	while (pos != value.end() and isdigit(*pos)) *pos++ -= '0';
 	value.erase(pos, value.end());
 }
-u::u(const str &s) {
+uinf::uinf(const str &s) {
 	value.insert(value.end(), s.begin(), s.end());
 	auto pos = value.begin();
 	while (pos != value.end() and isdigit(*pos)) *pos++ -= '0';
 	value.erase(pos, value.end());
 }
-u::u(u &&rhs) noexcept { value = std::move(rhs.value); }
+uinf::uinf(uinf &&rhs) noexcept { value = std::move(rhs.value); }
 
-u& u::operator = (u &&rhs) noexcept {
+uinf& uinf::operator = (uinf &&rhs) noexcept {
 	value = std::move(rhs.value);
 	return *this;
 }
 
-i32& u::operator [] (const u32 &index) { return value[index]; }
-const i32& u::operator [] (const u32 &index) const { return value[index]; }
+i32& uinf::operator [] (const u32 &index) { return value[index]; }
+const i32& uinf::operator [] (const u32 &index) const { return value[index]; }
 
 
 // ---------- arithmetic operators ----------
 
-u u::operator + (const u &rhs) const {
+uinf uinf::operator + (const uinf &rhs) const {
 	if (len() > rhs.len()) return rhs + *this;
 	// assume that len() <= rhs.len();
-	u ans(rhs);
+	uinf ans(rhs);
 	u32 up = 0; const u32 l = ans.len();
 	for (u32 i = 0; i < l; ++i) {
 		ans[i] += value[i] + up;
@@ -50,9 +50,9 @@ u u::operator + (const u &rhs) const {
 	return ans;
 }
 
-u u::operator - (const u &rhs) const {
+uinf uinf::operator - (const uinf &rhs) const {
 	assert(operator>(rhs));
-	u ans(*this);
+	uinf ans(*this);
 	u32 up = 0; const u32 l = len();
 	for (u32 i = 0; i < l; ++i) {
 		ans[i] -= rhs[i] + up;
@@ -62,8 +62,8 @@ u u::operator - (const u &rhs) const {
 	return ans;
 }
 
-u u::operator * (const u &rhs) const {
-	u ans; ans.resize(len() + rhs.len());
+uinf uinf::operator * (const uinf &rhs) const {
+	uinf ans; ans.resize(len() + rhs.len());
 	const u32 l1 = len(), l2 = rhs.len();
 	for (u32 i = 0; i < l1; ++i)
 		for (u32 j = 0; j < l2; ++j)
@@ -76,11 +76,10 @@ u u::operator * (const u &rhs) const {
 	return ans;
 }
 
-
 // ---------- comparison operators ----------
 
 // usage: equal to three-way comparison operator <=>
-i32 u::cmp(const u &rhs) const {
+i32 uinf::cmp(const uinf &rhs) const {
 	if (len() > rhs.len()) return 1;
 	if (len() < rhs.len()) return -1;
 	const u32 l = len();
@@ -90,9 +89,9 @@ i32 u::cmp(const u &rhs) const {
 	return 0;
 }
 
-bool u::operator == (const u &rhs) const { return cmp(rhs) == 0;}
-bool u::operator != (const u &rhs) const { return cmp(rhs) != 0; }
-bool u::operator < (const u &rhs) const { return cmp(rhs) < 0; }
-bool u::operator > (const u &rhs) const { return cmp(rhs) > 0; }
-bool u::operator <= (const u &rhs) const { return cmp(rhs) <= 0; }
-bool u::operator >= (const u &rhs) const { return cmp(rhs) >= 0; }
+bool uinf::operator == (const uinf &rhs) const { return cmp(rhs) == 0;}
+bool uinf::operator != (const uinf &rhs) const { return cmp(rhs) != 0; }
+bool uinf::operator < (const uinf &rhs) const { return cmp(rhs) < 0; }
+bool uinf::operator > (const uinf &rhs) const { return cmp(rhs) > 0; }
+bool uinf::operator <= (const uinf &rhs) const { return cmp(rhs) <= 0; }
+bool uinf::operator >= (const uinf &rhs) const { return cmp(rhs) >= 0; }
---
+++ src/uinf.hpp
@@ -0,0 +1,51 @@
+#ifndef Python_Interpreter_unsigned_integer
+#define Python_Interpreter_unsigned_integer
+
+#include "claim.hpp"
+
+class uinf {
+	// lower bit in lower index
+	// e.g. 1234567 = Vec<u32>{7, 6, 5, 4, 3, 2, 1}
+	Vec<i32> value;
+
+	void resize(u32 new_length) { resize(new_length); }
+	void remove_leading_zero() { while (not value.empty() and value.back() == 0) value.pop_back(); }
+	i32 cmp(const uinf &rhs) const; // usage: equal to three-way comparison operator <=>
+
+public:
+	const u32 len() const { return value.size(); }
+
+	uinf() { }
+	template <typename T> explicit uinf(T val);
+	explicit uinf(const char *s);
+	explicit uinf(const str &s);
+	uinf(const uinf &rhs) { operator=(rhs); }
+	uinf(uinf &&rhs) noexcept;
+
+	uinf& operator = (const uinf &rhs) = default;
+	uinf& operator = (uinf &&rhs) noexcept;
+
+	i32& operator [] (const u32 &index);
+	const i32& operator [] (const u32 &index) const;
+
+	uinf operator + (const uinf &rhs) const;
+	uinf operator - (const uinf &rhs) const;
+	uinf operator * (const uinf &rhs) const;
+	uinf operator / (const uinf &rhs) const;
+	uinf operator % (const uinf &rhs) const;
+
+	uinf& operator += (const uinf &rhs) { return operator=(operator+(rhs)); };
+	uinf& operator -= (const uinf &rhs) { return operator=(operator-(rhs)); };
+	uinf& operator *= (const uinf &rhs) { return operator=(operator*(rhs)); };
+	uinf& operator /= (const uinf &rhs) { return operator=(operator/(rhs)); };
+	uinf& operator %= (const uinf &rhs) { return operator=(operator%(rhs)); };
+
+	bool operator == (const uinf &rhs) const;
+	bool operator != (const uinf &rhs) const;
+	bool operator < (const uinf &rhs) const;
+	bool operator > (const uinf &rhs) const;
+	bool operator <= (const uinf &rhs) const;
+	bool operator >= (const uinf &rhs) const;
+};
+
+#endif

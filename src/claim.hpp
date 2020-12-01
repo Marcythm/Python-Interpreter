3d35f53f837327e2ef469c5055a4ed51795b97c6
Marcythm <marcythm@gmail.com>
2020-12-01 16:31:53 +0800

    modifiy type aliases

--- src/claim.hpp
+++ src/claim.hpp
@@ -9,13 +9,15 @@
 #include <cassert>
 #include <cstring>
 
-using i16 = short;
-using i32 = int;
-using i64 = long long;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 
-using u16 = unsigned short;
-using u32 = unsigned int;
-using u64 = unsigned long long;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
 
 using f64 = double;
 using f128 = long double;

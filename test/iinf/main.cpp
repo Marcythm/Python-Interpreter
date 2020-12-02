970fdada567d979a6da4cd658de48b3336afe462
Marcythm <marcythm@gmail.com>
2020-12-02 11:30:34 +0800

    add test: integer; test passed

---
+++ test/iinf/main.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "../../src/support/iinf.hpp"
+
+str a, b;
+
+i32 main() {
+	std::ifstream input; input.open("/Users/Marcythm/workspace/assignments/Python-Interpreter/test/iinf/input");
+	if (not input.is_open()) return 1;
+	input >> a >> b;
+	// std::cin >> a >> b;
+	/*if (op == '+') */ printf("%s\n", (iinf(a) + iinf(b)).tostr().c_str());
+	/*if (op == '-') */ printf("%s\n", (iinf(a) - iinf(b)).tostr().c_str());
+	/*if (op == '*') */ printf("%s\n", (iinf(a) * iinf(b)).tostr().c_str());
+	/*if (op == '/') */ printf("%s\n", (iinf(a) / iinf(b)).tostr().c_str());
+	/*if (op == '%') */ printf("%s\n", (iinf(a) % iinf(b)).tostr().c_str());
+
+	return 0;
+}
